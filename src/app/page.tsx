ea58faa52c9e8c9dc260d0225bbe81f13d066d3b
Bill Garrett <garrett@astralfrontier.org>
2024-12-10 11:36:34 -0800

    Sort character names on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,35 @@
 import loadCharacters from "@/lib/character-loader";
 import Link from "next/link";
 
+import styles from "./page.module.sass";
+
 export default function Home() {
   const characters = loadCharacters();
+
+  const sortedCharacters = characters.sort((a, b) => {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  });
+
   return (
     <>
       <div className="grid">
-        {characters.map((character) => (
+        {sortedCharacters.map((character) => (
           <div className="cell" key={character.id}>
-            <div className="card">
-              <div className="card-header">
-                <div className="card-header-title">
-                  <Link href={`/characters/${character.id}`}>
-                    {character.name}
-                  </Link>
+            <Link
+              className={styles.cardlink}
+              href={`/characters/${character.id}`}
+            >
+              <div className="card">
+                <div className="card-header">
+                  <div className="card-header-title">{character.name}</div>
+                </div>
+                <div className="card-content">
+                  {character.pitch || "No pitch for this character"}
                 </div>
               </div>
-              <div className="card-content">
-                {character.pitch || "No pitch for this character"}
-              </div>
-            </div>
+            </Link>
           </div>
         ))}
       </div>
