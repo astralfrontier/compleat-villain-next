4ed0df561eeac2c4e95b40ea5bf14412799e896a
Bill Garrett <garrett@astralfrontier.org>
2024-12-10 10:21:06 -0800

    Add a home link to the footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.scss";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Compleat Villain NEXT",
@@ -13,7 +14,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="container">{children}</div>
+        <footer className="footer">
+          <div className="columns is-centered">
+            <div className="column is-one-quarter">
+              <Link href={"/"}>Home</Link>
+            </div>
+          </div>
+        </footer>
+      </body>
     </html>
   );
 }
